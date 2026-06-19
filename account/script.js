447b2505538758e982ddc1572cd0c7ae44cd5bc7
khushwantdangi25 <khushwantdangi25@gmail.com>
2026-06-19 21:58:52 +0530

    account 5

--- account/script.js
+++ account/script.js
@@ -1,31 +1,57 @@
-let sub = document.querySelector("button");
+
+
+
+const form = document.querySelector("form");
 let inputs = document.querySelectorAll("input");
-sub.addEventListener("click" ,function(e){
-    e.preventDefault();
-    
+
+const users = {
+    user : [],
+    init : function(){
+        form.addEventListener("submit" , this.submitForm.bind(this));
+    },
+    submitForm: function(e){
+        e.preventDefault();
+        this.addAccount();
+    },
+    addAccount : function(){
+        this.user.push({
+            username : inputs[0].value,
+            bio : inputs[1].value,
+            profile : inputs[2].value,
+        });
+        form.reset();
+        this.userRender();
+    },
+    userRender: function(){
+       
     let cardElement = document.querySelector(".card");
-    cardElement.innerHTML = ""; 
-     let content = document.createElement("div")
-     content.classList.add("content");
 
-     let h2 = document.createElement("h2");
-     h2.textContent = inputs[0].value;
+    cardElement.innerHTML = "";
 
-     let h3 = document.createElement("h3");
-     h3.textContent = inputs[1].value;
+    this.user.forEach(function(e) {
+        let content = document.createElement("div");
+        content.classList.add("content");
 
-     let imge = document.createElement("div");
-     imge.classList.add("img")
+        let h2 = document.createElement("h2");
+        h2.textContent = e.username;
 
-    let im = document.createElement("img");
-    im.setAttribute("src" ,inputs[2].value);
+        let h3 = document.createElement("h3");
+        h3.textContent = e.bio;
 
-    imge.appendChild(im);
-    document.querySelector(".card").appendChild(imge);
-    content.appendChild(h2);
-    content.appendChild(h3);
-     document.querySelector(".card").appendChild(content);
-    
-     document.querySelector("form").reset();
-
-});
+        let image = document.createElement("img");
+        image.src = e.profile;
+        
+         let img = document.createElement("div");
+        content.classList.add("img");
+        img.appendChild(image);
+         content.appendChild(img);
+        content.appendChild(h2);
+        content.appendChild(h3);
+         
+       
+        cardElement.appendChild(content);
+    });
+    },
+    removeAccount : function(){},
+};
+users.init();
